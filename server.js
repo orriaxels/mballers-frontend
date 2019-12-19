1424019b241df155c75e3768ed5a3974c920b53e
Orri Axelsson <orriaxels@gmail.com>
2019-12-19 02:00:25 +0000

    heroku fix

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/mball-web'));
+app.use(express.static(__dirname + '/dist/mballWeb'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/mball-web/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/mballWeb/index.html'));
 });
 
 // default Heroku PORT
